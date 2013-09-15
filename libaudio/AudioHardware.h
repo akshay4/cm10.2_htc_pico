402be57c957a76e847c5196521ac991f21169584
Ricardo Soares <ricjoi@hotmail.com>
2013-09-15 09:58:37 +0100

    remove lpa changes

--- libaudio/AudioHardware.h
+++ libaudio/AudioHardware.h
@@ -69,15 +69,12 @@ using android::Condition;
 #define MBADRC_DISABLE 0xFFEF
 #define SRS_ENABLE 0x0020
 #define SRS_DISABLE 0xFFDF
-#define LPA_BUFFER_SIZE 512*1024
+#define LPA_BUFFER_SIZE 480*1024
 #define BUFFER_COUNT 2
 
 #define AGC_ENABLE     0x0001
 #define NS_ENABLE      0x0002
 #define TX_IIR_ENABLE  0x0004
-
-
-#define AUDIO_PARAMETER_KEY_FLUENCE_TYPE "fluence"
 
 struct eq_filter_type {
     int16_t gain;
